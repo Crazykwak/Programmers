686c9e5754a1438715b7067dfb2f29daf6c50a5c
Crazykwak <kwj1830@naver.com>
2022-05-02 21:56:50 +0900

    AddDigits

--- src/AlgorithmApp.java
+++ src/AlgorithmApp.java
@@ -1,11 +1,11 @@
 import level1.*;
 
-import java.util.Arrays;
-
 public class AlgorithmApp {
     public static void main(String[] args) {
-        MinNumDelete min = new MinNumDelete();
-        int[] a = {4,3,2,1};
-        System.out.println(Arrays.toString(min.solution(a)));
+
+        AddDigits addDigits = new AddDigits();
+
+        int answer = addDigits.Solution(123);
+        System.out.println("answer = " + answer);
     }
 }
---
+++ src/level1/AddDigits.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class AddDigits {
+
+    public int Solution(int n) {
+        int answer = 0;
+
+        while (n >= 1) {
+            answer += n % 10;
+            n =  n / 10;
+        }
+        return answer;
+    }
+}

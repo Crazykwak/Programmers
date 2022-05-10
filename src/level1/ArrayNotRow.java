dc39d692e32dba679e9d65017ff88294af65d1e6
Crazykwak <kwj1830@naver.com>
2022-05-10 21:23:52 +0900

    Array int not row

--- src/level1/ArrayNotRow.java
+++ src/level1/ArrayNotRow.java
@@ -1,2 +1,20 @@
-package level1;public class ArrayNotRow {
+package level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayNotRow {
+    public int[] solution(int []arr) {
+        ArrayList al = new ArrayList();
+
+        int tmpN = 10;
+        for (int i = 0; i < arr.length; i++) {
+            if (tmpN != arr[i]) {
+                al.add(arr[i]);
+            }
+            tmpN = arr[i];
+        }
+
+        return al.stream().mapToInt(i -> (int) i).toArray();
+    }
 }

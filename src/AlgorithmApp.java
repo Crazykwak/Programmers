f041f9f097a8eaf556e5c5994db5debf3e1db695
Crazykwak <kwj1830@naver.com>
2022-05-19 09:54:57 +0900

    Make Minimum Number

--- src/AlgorithmApp.java
+++ src/AlgorithmApp.java
@@ -1,5 +1,6 @@
 import level1.*;
 import level2.JadenCase;
+import level2.MakeMinimumNumber;
 import practice.Member;
 import practice.MyComparator;
 
@@ -10,12 +11,12 @@ public class AlgorithmApp {
     public static void main(String[] args) {
         //test 하는 곳
 
-
-
-        String s = "for  the last week";
-        JadenCase jc = new JadenCase();
-        String solution = jc.solution(s);
+        int[] a = {1,2};
+        int[] b = {3,4};
+        MakeMinimumNumber mm = new MakeMinimumNumber();
+        int solution = mm.solution(a, b);
         System.out.println("solution = " + solution);
+
 
     }
 

47074003bd8dde0a9930c0f86a56fa6b00da7e98
Crazykwak <kwj1830@naver.com>
2022-05-25 09:36:32 +0900

    H-Index 2

--- src/level2/HIndex.java
+++ src/level2/HIndex.java
@@ -18,4 +18,22 @@ public class HIndex {
 
         return answer;
     }
+
+    public int solution2(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+
+            int min = Math.min(citations[i], citations.length - i);
+            if(min >= answer) {
+                answer = min;
+            }
+
+        }
+        return answer;
+    }
+
+
 }

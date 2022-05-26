9826064977e61d5b73b93ffc6f9329f1b11fb4d4
Crazykwak <kwj1830@naver.com>
2022-05-26 20:41:57 +0900

    Fatigue

--- src/AlgorithmApp.java
+++ src/AlgorithmApp.java
@@ -8,19 +8,11 @@ public class AlgorithmApp {
     public static void main(String[] args) {
         //test 하는 곳
 
-//        int[][] d = {{80, 20}, {50, 40}, {30, 10}};
-//        int k = 80;
-//        Fatigue fatigue = new Fatigue();
-//        int solution = fatigue.solution(k, d);
-//        System.out.println("solution = " + solution);
-
-        ///
-
-        Integer[] boxes = {5,1,4,6,5 ,4,3,2,1,7};
-        Box box = new Box();
-        int i = box.paveBox(boxes);
-        System.out.println("i = " + i);
-
+        int[][] d = {{80, 20}, {50, 10}, {50, 10}, {50, 10}, {10, 10}, {10, 10}};
+        int k = 40;
+        Fatigue fatigue = new Fatigue();
+        int solution = fatigue.solution(k, d);
+        System.out.println("solution = " + solution);
 
     }
 }
--- src/level2/Fatigue.java
+++ src/level2/Fatigue.java
@@ -3,44 +3,25 @@ package level2;
 import java.util.*;
 
 public class Fatigue {
+    static int answer = 0;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        int min = getMin(dungeons);
-        int tri = 0;
         HashSet<Integer> went = new HashSet<>();
-        answer = dun(k, dungeons, min, tri, went);
+        return dun(k, dungeons, went, 0);
 
 
-        return answer;
     }
 
-    private int getMin(int[][] dungeons) {
-        int min = dungeons[0][0];
-        for (int i = 0; i < dungeons.length; i++) {
-            if(dungeons[i][0] <= min) {
-                min = dungeons[i][0];
-            }
-        }
-        return min;
-    }
-
-    int dun(int k, int[][] dungeon, int min, int tri, HashSet<Integer> went) {
-        if(k < min) {
-            return tri;
-        }
-
+    int dun(int k, int[][] dungeon, HashSet<Integer> went, int high) {
         for (int i = 0; i < dungeon.length; i++) {
             if(!went.contains(i) && k >= dungeon[i][0]) {
              went.add(i);
-             int k_ = k- dungeon[i][1];
-             int tri_ = tri+1;
-                int dun = dun(k_, dungeon, min, tri_, went);
-
+             dun(k - dungeon[i][1], dungeon, went, high+1);
+             went.remove(i);
             }
         }
-
-        return went.size();
+        answer = Math.max(answer, high);
+        return answer;
     }
-
 
 }

970483f9a5e7a5c7f1f718c2b8c7165b7ac67c66
Crazykwak <kwj1830@naver.com>
2022-05-27 01:01:52 +0900

    Delivery

---
+++ src/level2/Delivery.java
@@ -0,0 +1,54 @@
+package level2;
+
+import java.util.*;
+
+public class Delivery {
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        int[] tmp = new int[N];
+        int max = 5000001;
+        tmp[0] = 0;
+        for (int i = 1; i < tmp.length; i++) {
+            tmp[i] = max;
+        }
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(i==j) graph[i][j] = 0;
+                else graph[i][j] = max;
+            }
+        }
+        for (int[] ints : road) {
+            int from = ints[0]-1;
+            int to = ints[1]-1;
+            if(graph[from][to] >= ints[2]) {
+                graph[from][to] = graph[to][from] = ints[2];
+            }
+        }
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> a[0]- b[0]);
+        Set<Integer> visited = new HashSet<>();
+        queue.add(new int[]{0,0});
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int u = poll[1];
+            if(visited.contains(u)) {
+                continue;
+            }
+
+            visited.add(u);
+            for (int i = 0; i < N; ++i) {
+                if(tmp[i] > tmp[u] + graph[u][i]){
+                    tmp[i] = tmp[u] + graph[u][i];
+                    queue.add(new int[] {tmp[i], i});
+                }
+            }
+
+        }
+        System.out.println("tmp = " + Arrays.toString(tmp));
+        long count = Arrays.stream(tmp).filter(e -> e <= K).count();
+        answer = (int) count;
+        return answer;
+    }
+}

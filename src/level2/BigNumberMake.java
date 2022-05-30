9a38f9f513da63ea97ef8100ef5025505ae287b9
Crazykwak <kwj1830@naver.com>
2022-05-30 11:33:53 +0900

    Big Number Make

--- src/level2/BigNumberMake.java
+++ src/level2/BigNumberMake.java
@@ -2,16 +2,32 @@ package level2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class BigNumberMake {
-
-    ArrayList<String> list = new ArrayList<>();
-
     public String solution(String number, int k) {
 
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int answerLen = number.length() - k;
+        int start = 0;
+
+        while (start < number.length() && sb.length() != answerLen) {
+            int left = k + sb.length() + 1;
+            int max = 0;
+            for(int j = start; j < left; j++) {
+                if(Integer.valueOf(number.charAt(j)) == 9) {
+                    start = j + 1;
+                    break;
+                }
+                if(max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            sb.append(max);
+        }
+
+        return sb.toString();
     }
-
-
-
 }

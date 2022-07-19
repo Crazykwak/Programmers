754f56a074197a9d748263f80a4a6ea5c7ded437
Crazykwak <kwj1830@naver.com>
2022-07-19 09:46:13 +0900

    insertion sort

--- src/AlgorithmApp.java
+++ src/AlgorithmApp.java
@@ -13,13 +13,12 @@ public class AlgorithmApp {
     public static void main(String[] args) {
         //test 하는 곳
 
-        int rows = 6;
-        int column = 8;
-        int[][] queries = {{1, 1, 2, 2}, {2, 3, 3, 4}, {1, 2, 3, 4}, {1, 1, 3, 4}, {2, 2, 3, 5}};
+        int[] a = {1, 2, 43, 100, 21};
 
-        RotatedEdge rotatedEdge = new RotatedEdge();
-        int[] solution = rotatedEdge.solution(rows, column, queries);
-        System.out.println("solution = " + Arrays.toString(solution));
+        InsertionSort sort = new InsertionSort();
+        int[] ints = sort.insertionSort(a);
+        System.out.println("ints = " + Arrays.toString(ints));
+
 
     }
 }

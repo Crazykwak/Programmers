356b0872294c18a0147ed0fc38c7a4b40fef00e9
Crazykwak <kwj1830@naver.com>
2022-08-08 22:42:25 +0900

    백춘 우선순위큐 최소힙 최대힙 절대값힙

--- src/backjun/HeapMin.java
+++ src/backjun/HeapMin.java
@@ -14,11 +14,28 @@ public class HeapMin {
 
         int n = Integer.parseInt(br.readLine());
 
-        // 최대힙
-//        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // 절대값힙
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                long abs1 = Math.abs(o1);
+                long abs2 = Math.abs(o2);
+
+                if (abs1 > abs2) {
+                    return 1;
+                } else if (abs1 < abs2) {
+                    return -1;
+                } else {
+                    return o1 < o2 ? -1 : 1;
+                }
+            }
+        });
 
         // 최소힙
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+//        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        //최대힙
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());

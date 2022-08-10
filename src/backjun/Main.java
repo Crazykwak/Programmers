65427530782ad9aa3067c68f3b0e33095bf627bc
Crazykwak <kwj1830@naver.com>
2022-08-10 15:49:00 +0900

    백준 기본 세팅

---
+++ src/backjun/Main.java
@@ -0,0 +1,18 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
+    }
+}

bfff6477fbeb4005677aa5f524f4559e58d81e4a
Crazykwak <kwj1830@naver.com>
2022-11-22 12:43:09 +0900

    프로그래머스 3레벨 등대

--- src/AlgorithmApp.java
+++ src/AlgorithmApp.java
@@ -18,10 +18,27 @@ public class AlgorithmApp {
     public static void main(String[] args) throws IOException {
         //test 하는 곳
 
-        int[][] board = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};
+//        int n = 10;
+//        int[][] lighthouse = {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}};
+        //answer = 2;
 
-        MostBigSquare mostBigSquare = new MostBigSquare();
-        int solution = mostBigSquare.solution(board);
+//        int n = 8;
+//        int[][] lighthouse = {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}};
+        //answer = 3;
+
+//        int n = 6;
+//        int[][] lighthouse = {{5, 1}, {1, 2}, {1, 4}, {1, 3}, {2, 6}};
+        //answer = 2;
+
+//        int n = 5;
+//        int[][] lighthouse = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+        //answer = 2;
+
+        int n = 2;
+        int[][] lighthouse = {{1,2}};
+
+        Lighthouse lighthouse1 = new Lighthouse();
+        int solution = lighthouse1.solution(n, lighthouse);
         System.out.println("solution = " + solution);
 
     }
--- src/level3/Lighthouse.java
+++ src/level3/Lighthouse.java
@@ -1,55 +1,98 @@
 package level3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lighthouse {
 
-    int answer = 1000001;
 
     public int solution(int n, int[][] lighthouse) {
 
-        boolean[] on = new boolean[n];
+        if (n == 2) {
+            return 1;
+        }
 
-        dfs(n, lighthouse,1, on, 0);
+        int answer = 0;
+        Tree[] trees = new Tree[n];
+
+        for (int i = 0; i < n; i++) {
+            trees[i] = new Tree(i);
+        }
+
+        // tree 만들기
+        for (int[] light : lighthouse) {
+            int a = light[0];
+            int b = light[1];
+            trees[a-1].addChild(trees[b-1]);
+            trees[b-1].addChild(trees[a-1]);
+        }
+
+        // 등대를 켜는 동작
+        boolean[] visited = new boolean[n];
+        trees[n-1].onOff(visited);
+
+        visited = new boolean[n];
+        trees[n-1].onOff(visited);
+
+        for (Tree tree : trees) {
+            answer += tree.on ?  1 : 0;
+        }
+
+        for (Tree tree : trees) {
+            System.out.println((tree.name+1));
+            for (Tree child : tree.children) {
+                System.out.println((tree.name+1) + "의 자식들 " + (child.name+1));
+            }
+            System.out.println((tree.name+1) + "의 자식 노드 길이 " + tree.children.size());
+            System.out.println("tree.on = " + tree.on);
+        }
 
         return answer;
     }
 
-    private void dfs(int n, int[][] lighthouse, int count, boolean[] on, int start) {
-        if (count >= n/2) {
-            answer = Math.min(count, answer);
-            return;
+    private class Tree {
+        int name;
+        boolean on;
+        List<Tree> children = new ArrayList<>();
+
+        public void addChild(Tree tree) {
+            children.add(tree);
         }
 
-        for (int i = start; i < n; i++) {
-            if (!on[i]) {
-                on[i] = true;
-            }
-
-            if(check(lighthouse, on) && count < answer) {
-                answer = count;
-                System.out.println(" = ");
-                System.out.println("count = " + count);
-                for (boolean b : on) {
-                    System.out.println("b = " + b);
-                }
+        public void onOff(boolean[] visited) {
+            if (visited[name]) {
                 return;
             }
-            dfs(n, lighthouse, count+1, on, i+1);
-            on[i] = false;
-        }
 
-    }
-
-    public boolean check(int[][] lighthouse, boolean[] on) {
-        for (int[] light : lighthouse) {
-            int a = light[0]-1;
-            int b = light[1]-1;
-
-            if (!(on[a] || on[b])) {
-                return false;
+            if (children.size() == 1) {
+                on = false;
+                visited[name] = true;
+                children.get(0).onOff(visited);
+                return;
             }
+
+            visited[name] = true;
+
+            for (int i = 0; i < children.size(); i++) {
+                Tree child = children.get(i);
+                child.onOff(visited);
+            }
+
+            on = checkOn() ? false : true;
         }
 
-        return true;
+        public boolean checkOn() {
+            for (Tree child : children) {
+                if (!child.on) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Tree(int name) {
+            this.name = name;
+        }
     }
 
 }

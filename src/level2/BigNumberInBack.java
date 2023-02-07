e7985742236a9dbecdde9e0328b7da51cee13009
Crazykwak <kwj1830@naver.com>
2023-02-07 16:04:49 +0900

    프로그래머스 2레벨 뒤에 있는 큰 수 찾기

--- src/level2/BigNumberInBack.java
+++ src/level2/BigNumberInBack.java
@@ -1,5 +1,6 @@
 package level2;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class BigNumberInBack {
@@ -19,10 +20,6 @@ public class BigNumberInBack {
             pq.add(new int[] {target, i});
         }
 
-        while (!pq.isEmpty()) {
-            answer[pq.poll()[1]] = -1;
-        }
-
-        return answer;
+        return Arrays.stream(answer).map(e -> e == 0 ? -1 : e).toArray();
     }
 }

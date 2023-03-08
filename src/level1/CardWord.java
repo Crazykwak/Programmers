9141cd1a8c35f8fc95febf1d7701f81edaa951c5
Crazykwak <kwj1830@naver.com>
2023-03-08 20:59:56 +0900

    프로그래머스 1레벨 카드 뭉치 (수정)

--- src/level1/CardWord.java
+++ src/level1/CardWord.java
@@ -1,32 +1,26 @@
 package level1;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class CardWord {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
 
-        Queue<String> left = new LinkedList();
-        Queue<String> right = new LinkedList();
-
-        for (String s : cards1) {
-            left.add(s);
-        }
-        for (String s : cards2) {
-            right.add(s);
-        }
+        Queue<String> left = new LinkedList(List.of(cards1));
+        Queue<String> right = new LinkedList(List.of(cards2));
 
         for (int i = 0; i < goal.length; i++) {
             String target = goal[i];
             String leftWord = left.peek();
             String rightWord = right.peek();
 
-            if (!left.isEmpty() && target.equals(leftWord)) {
+            if (target.equals(leftWord)) {
                 left.poll();
                 continue;
             }
 
-            if (!right.isEmpty() && target.equals(rightWord)) {
+            if (target.equals(rightWord)) {
                 right.poll();
                 continue;
             }
